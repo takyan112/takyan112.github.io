4c7dbc094e5dd5ef6aeec7cb13dfb9f1a548586d
takyan112 <bryantse112@gmail.com>
2023-03-31 00:38:42 +0800

    random color after each bounce

--- canvas.js
+++ canvas.js
@@ -4,7 +4,13 @@ function setElementPosition(el, x, y) {
 	el.style.top = y + 'px';
 }
 
+function randomColor() {
+	var colors = ['aqua','blue','fuchsia','gray','green','lime','maroon','navy','olive','purple','red','silver','teal','yellow']
+	return colors[Math.floor(Math.random()*colors.length)]
+}
+
 var logo = document.getElementById('DVD_logo');
+var svg = document.getElementById('DVD_svg');
 var t0 = 0;
 var x = 0;
 var y = 0;
@@ -20,13 +26,15 @@ function loop(t) {
 	
 	x += vx * dt;
 	y += vy * dt; 
-	if (x < 0 || w-logo.width <= x) {
+	if (x < 0 || w-svg.width <= x) {
 		vx *= -1;
 		x += vx * dt;
+		svg.style = `filter: drop-shadow(-100px 0px 0px ${randomColor()});`;
 	}
-	if (y < 0 || h-logo.height <= y) {
+	if (y < 0 || h-svg.height <= y) {
 		vy *= -1;
 		y += vy * dt;
+		svg.style = `filter: drop-shadow(-100px 0px 0px ${randomColor()});`;
 	}
 	
 	setElementPosition(logo, x, y);
